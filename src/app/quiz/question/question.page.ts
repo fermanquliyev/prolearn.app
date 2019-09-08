b838431cb8f473602021adf0e2884af25cd498e8
fermanquliyev <33841624+fermanquliyev@users.noreply.github.com>
2019-09-08 23:33:54 +0400

    Quiz

--- src/app/quiz/question/question.page.ts
+++ src/app/quiz/question/question.page.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { QuizServiceProxy, QuestionModel } from 'src/shared/service-proxies/service-proxies';
 import { ActivatedRoute } from '@angular/router';
-import { ToastController, AlertController } from '@ionic/angular';
+import { ToastController, AlertController, NavController, IonRadioGroup } from '@ionic/angular';
+import * as moment from 'moment';
+import { TouchSequence } from 'selenium-webdriver';
 @Component({
   selector: 'app-question',
   templateUrl: './question.page.html',
@@ -13,11 +15,15 @@ export class QuestionPage implements OnInit {
   questions: QuestionModel[];
   currentQuestion: QuestionModel;
   image: HTMLElement;
+  time: moment.Duration = moment.duration();
+  stopTime: boolean;
+  @ViewChild('ionRadioGroup', {static: false}) ionRadioGroup: IonRadioGroup;
   constructor(
     private quizService: QuizServiceProxy,
     private activeRoute: ActivatedRoute,
     private toastController: ToastController,
-    private alertController: AlertController
+    private alertController: AlertController,
+    private navCtrl: NavController
   ) { }
 
   ngOnInit() {
@@ -53,7 +59,34 @@ export class QuestionPage implements OnInit {
     });
   }
 
+  ionViewDidEnter() {
+    this.time = moment.duration(this.questions.length * 60000);
+    this.stopTime = false;
+    this.timer();
+  }
+
+  ionViewWillLeave() {
+    this.stopTime = true;
+  }
+
+  timer() {
+    setTimeout(() => {
+      this.time = this.time.subtract(1, 'seconds');
+      if (this.stopTime) {
+        return;
+      }
+      if (this.time.asSeconds() === 0) {
+        this.checkAnswer();
+      } else {
+        this.timer();
+      }
+    }, 1000);
+  }
+
   getQuestion() {
+    if (this.ionRadioGroup) {
+      this.ionRadioGroup.value = -1;
+    }
     this.currentQuestion = this.questions[this.index];
   }
 
@@ -71,27 +104,36 @@ export class QuestionPage implements OnInit {
     }
   }
 
-  checkAnswer() {
-    const length = this.currentQuestion.answers.filter(x => x.isCorrect !== x.selected).length;
-    if (length === 0) {
-      this.alertController.create({
-        header: 'Cavabınız doğrudur!',
-        message: this.currentQuestion.correctAnswerDescription,
-        animated: true
-      }).then(alert => alert.present());
-    } else {
-      this.alertController.create({
-        header: 'Cavabınız yanlışdır!',
-        message: this.currentQuestion.correctAnswerDescription,
-        animated: true
-      }).then(alert => alert.present());
-    }
+  async checkAnswer() {
+    const wrongAnsweredQuestions = this.questions.filter(x => x.answers.filter(y => y.isCorrect !== y.selected).length > 0);
+    const alertHeader = `${this.questions.length} sual, ${this.questions.length - wrongAnsweredQuestions.length} doğru, ${wrongAnsweredQuestions.length} yanlış`;
+    let alertMessage = '<div style="border:1px solid;">';
+    wrongAnsweredQuestions.forEach((question, index) => {
+      alertMessage += `<p> ${index + 1}: ${this.encode(question.questionDescription)}
+      <br>
+      Sizin cavab: ${question.answers.filter(x => x.selected).map(x => x.answerText).reduce((a, b) => a + ', ' + b, '')}
+      <br>
+      Doğru cavab: ${question.answers.filter(x => x.isCorrect).map(x => x.answerText).reduce((a, b) => a + ', ' + b)}
+      <br>
+      İzah: ${question.correctAnswerDescription !== null ? question.correctAnswerDescription : question.answers.filter(x => x.isCorrect).map(x => x.answerText).reduce((a, b) => a + ', ' + b)}
+      </p>`;
+    });
+    alertMessage += '</div>';
+    const alert = await this.alertController.create({
+      header: alertHeader,
+      message: alertMessage,
+      animated: true,
+      cssClass: 'width100',
+    });
+    await alert.present();
+    alert.onDidDismiss().then(result => this.back());
   }
 
   radioChange(event) {
-    this.currentQuestion.answers.forEach(a => a.selected = false);
     if (event.detail.value >= 0) {
+      this.currentQuestion.answers.forEach(a => a.selected = false);
       this.currentQuestion.answers[event.detail.value].selected = true;
+      console.table(this.currentQuestion.answers);
     }
   }
 
@@ -106,5 +148,9 @@ export class QuestionPage implements OnInit {
       '>': '&gt;'
     };
     return tagsToReplace[tag] || tag;
+  }
+
+  back() {
+    this.navCtrl.back();
   }
 }
